e18dac0bd6b375d332ebc7a7a57d5ed3aa0d9f3b
MarlonAntonio <113026437+marlonamr@users.noreply.github.com>
2022-10-19 08:42:56 -0300

    nova class

---
+++ src/testegithub/Pessoa.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testegithub;
+
+/**
+ *
+ * @author alunodev11
+ */
+public class Pessoa {
+    private String nome;
+    private String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
+    
+    
+}
